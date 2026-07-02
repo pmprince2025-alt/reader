f236c77c4da8c65c22bfeaddca1cd546658e336f
Prince Meher <pmprince2025@gmail.com>
2026-07-02 12:49:28 +0530

    Fix: remove duplicate Room type converters, add launcher icon XML resources

--- core/core-database/src/main/java/com/folio/core/database/FolioDatabase.kt
+++ core/core-database/src/main/java/com/folio/core/database/FolioDatabase.kt
@@ -2,14 +2,12 @@ package com.folio.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(
     entities = [BookEntity::class, ShelfEntity::class, BookShelfCrossRef::class, ReadingProgressEntity::class],
     version = 1,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class FolioDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
     abstract fun shelfDao(): ShelfDao
